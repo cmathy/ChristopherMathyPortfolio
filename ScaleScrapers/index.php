c669e99fafe000971c9905eefc7c6037a971b24b
Christopher Ryan Mathy <cmathy@Christophers-MacBook-Pro-2.local>
2016-03-09 16:17:23 -0600

    first commit

---
+++ ScaleScrapers/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Christopher Mathy</title>
+	<link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+<body>
+	<div id="container">
+		<div id="header">
+			<h1>Christopher Mathy</h1>
+			<h3>Programmer | Web Designer | Game Designer</h3>
+		</div>
+		<div id="rule"></div>
+		<?php include '../phpContent/ScaleScrapers.php';?>
+	</div>
+</body>
+</html>
---
+++ phpContent/ScaleScrapers.php
@@ -0,0 +1,25 @@
+<div id="banner">
+	<img id="bannerPic" src="../pics/SS5.jpg">
+</div>
+<div id="bannerTxtBg">
+	<h2>ScaleScrapers</h2>
+</div>
+<div id="desc">
+	<h4>Mobile Platformer Game</h4></br>
+
+	<a href="https://play.google.com/store/apps/details?id=com.WistfulBoards.ScaleScrapers&hl=en">
+		Download for free from the Google Play Store!</a></br>
+
+	<h4>Made: </h4>
+	<p>Fall Semester 2015</p></br>
+
+	<h4>Team Size: </h4>
+	<p>4</p></br>
+
+	<h4>My Role: </h4>
+	<p>Lead Designer/Lead Programmer</p></br>
+
+	<h4>Description:</h4>
+	<p>ScaleScrapers is a simple yet fun mobile game developed for Android 
+		mobile devices.</p></br>
+</div>
