5852b9235fb70e597911989a0ff1d01997df332f
Christopher Mathy <cmathy@mail.bradley.edu>
2016-03-09 16:47:32 -0600

    adding other folders?

---
+++ 365/pastaparty/contact/emailscript.js
@@ -0,0 +1,4 @@
+function submitForm() {
+    document.getElementById("emailForm").reset();
+    alert("Thank you for your email! A copy was sent to your email address");
+}
---
+++ Advanced-web-design/README.md
@@ -0,0 +1,3 @@
+# Advanced-Web-Design
+
+go to http://christophermathy.com/Advanced-Web-Design/ to see the webpage
