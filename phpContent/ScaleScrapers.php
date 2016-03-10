202bf6b23d8756bec8ca4761df86981f66ea615e
Christopher Mathy <cmathy@mail.bradley.edu>
2016-03-10 17:01:22 -0600

    adding updated scalescrapers and css

--- phpContent/ScaleScrapers.php
+++ phpContent/ScaleScrapers.php
@@ -5,10 +5,10 @@
 	<h2>ScaleScrapers</h2>
 </div>
 <div id="desc">
-	<h4>Mobile Platformer Game</h4></br>
+	<a href="https://play.google.com/store/apps/details?id=com.WistfulBoards.ScaleScrapers&hl=en" class="button">
+		Download for free from the Google Play Store!</a>
 
-	<a href="https://play.google.com/store/apps/details?id=com.WistfulBoards.ScaleScrapers&hl=en">
-		Download for free from the Google Play Store!</a></br>
+	<h4>Mobile Platformer Game</h4></br>
 
 	<h4>Made: </h4>
 	<p>Fall Semester 2015</p></br>
@@ -21,5 +21,28 @@
 
 	<h4>Description:</h4>
 	<p>ScaleScrapers is a simple yet fun mobile game developed for Android 
-		mobile devices.</p></br>
+		mobile devices over the course of one semester. Avoid obstacles and 
+		use power ups to make it as high as you can.</p></br>
+
+	<h4>Software Used:</h4>
+	<ul>
+		<li>Unity</li>
+		<li>MonoDevelop</li>
+		<li>Adobe Photoshop</li>
+		<li>Adobe Illustrator</li>
+		<li>Trello</li>
+	</ul>
+
+	<h4>Skills Learned/Skilles Sharpened:</h4>
+	<ul>
+		<li>Team leadership</li>
+		<li>Team communication skills</li>
+		<li>SCRUM process</li>
+		<li>Scheduling</li>
+		<li>Working with teammates with different creative visions</li>
+		<li>C# programming</li>
+		<li>Adobe Creative Suite</li>
+		<li>Unity</li>
+		<li>File management/Version Control</li>
+	</ul>
 </div>
